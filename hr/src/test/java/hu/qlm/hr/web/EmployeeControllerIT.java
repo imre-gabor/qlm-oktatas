201be9684456285f424eebe1198e8368be04f9d0
imre-gabor <gabor@aut.bme.hu>
2022-10-07 10:04:42 +0200

    do JWT auth. at integration test

--- hr/src/test/java/hu/qlm/hr/web/EmployeeControllerIT.java
+++ hr/src/test/java/hu/qlm/hr/web/EmployeeControllerIT.java
@@ -3,6 +3,8 @@ package hu.qlm.hr.web;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -12,6 +14,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hu.qlm.hr.model.Employee;
+import hu.qlm.hr.security.LoginDto;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -23,6 +26,26 @@ public class EmployeeControllerIT {
 	@Autowired
 	WebTestClient webTestClient;
 	
+	String jwt;
+	
+	@BeforeEach
+	public void init() {
+		LoginDto loginDto = new LoginDto();
+		loginDto.setUsername("test");
+		loginDto.setPassword("pass");
+		
+		jwt = webTestClient
+			.post()
+			.uri("/api/login")
+			.bodyValue(loginDto)
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody(String.class)
+			.returnResult()
+			.getResponseBody();
+	}
+	
 	
 	@Test
 	void testThatCreatedEmployeeIsListed() throws Exception {
@@ -33,6 +56,7 @@ public class EmployeeControllerIT {
 			.post()
 			.uri(BASE_URI)
 			.bodyValue(new Employee(null, "name", "job", 300000, LocalDateTime.of(2015, 10, 1, 8, 0)))
+			.headers(headers -> headers.setBearerAuth(jwt))
 			.exchange()
 			.expectStatus()
 			.isOk()
